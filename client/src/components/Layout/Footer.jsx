123494c7e22b34201fa10e1b1a0249a4fb2fa34b
Saurabh Khanka <164884123+Saurabhkhanka@users.noreply.github.com>
2025-07-19 21:32:57 +0530

    Update Footer.jsx

--- client/src/components/Layout/Footer.jsx
+++ client/src/components/Layout/Footer.jsx
@@ -11,7 +11,7 @@ const Footer = () => {
           <div className="col-md-4 mb-3">
             <h5>DEV-STREET</h5>
             <p className=" ">
-              A full-stack developer passionate about building clean & user-friendly web apps.
+              “The intersection of creativity & code.”
             </p>
           </div>
 
